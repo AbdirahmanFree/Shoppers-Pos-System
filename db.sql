d6ebf13df4b8bc1efee26b1ee4f2eb70f7f6853c
AbdirahmanFree <abdirahmanm.abdulahi@gmail.com>
2025-09-22 23:16:11 -0400

    final changes

--- db.sql
+++ db.sql
@@ -1,14 +1,12 @@
 create table store (
-   store_id   number
-      generated always as identity,
+   store_id   number generated always as identity,
    store_name varchar2(100) not null,
    address    varchar2(200),
    constraint pk_store primary key ( store_id )
 );
 
 create table product (
-   product_id       number
-      generated always as identity,
+   product_id       number generated always as identity,
    product_name     varchar2(100) not null,
    product_category varchar2(50),
    price            number(10,2) default 0,
@@ -17,8 +15,7 @@ create table product (
 );
 
 create table customer (
-   customer_id    number
-      generated always as identity,
+   customer_id    number generated always as identity,
    customer_name  varchar2(100) not null,
    phone_number   varchar2(15),
    email          varchar2(100),
@@ -27,8 +24,7 @@ create table customer (
 );
 
 create table employee (
-   employee_id   number
-      generated always as identity,
+   employee_id   number generated always as identity,
    employee_name varchar2(100) not null,
    role          varchar2(50) not null,
    hire_date     date not null,
@@ -39,58 +35,46 @@ create table employee (
 
 
 create table transaction (
-   transaction_id   number primary key not null
-      generated always as identity,
+   transaction_id   number generated always as identity,
    employee_id      number not null,
    store_id         number not null,
    customer_id      number not null,
    payment_method   varchar(30),
    transaction_date date not null,
    total_amount     number check ( total_amount > 0 ),
-   constraint fk_tx_employee foreign key ( employee_id )
-      references employee ( employee_id ),
-   constraint fk_tx_store foreign key ( store_id )
-      references store ( store_id ),
-   constraint fk_tx_customer foreign key ( customer_id )
-      references customer ( customer_id )
+   constraint pk_tansaction primary key ( transaction_id ),
+   constraint fk_tx_employee foreign key ( employee_id ) references employee ( employee_id ),
+   constraint fk_tx_store foreign key ( store_id ) references store ( store_id ),
+   constraint fk_tx_customer foreign key ( customer_id ) references customer ( customer_id )
 )
 
 create table lineitem (
-   transaction_id   number not null
-      generated always as identity,
+   transaction_id   number generated always as identity,
    line_no          number not null,
    product_id       number not null,
    quantity         number not null check ( quantity > 0 ),
    discount_applied number(5,2) default 0 check ( discount_applied >= 0 ),
-   constraint pk_lineitem primary key ( transaction_id,
-                                        line_no ),
-   constraint fk_li_tx foreign key ( transaction_id )
-      references transaction ( transaction_id )
-         on delete cascade,
-   constraint fk_li_prod foreign key ( product_id )
-      references product ( product_id )
+   constraint pk_lineitem primary key ( transaction_id, line_no ),
+   constraint fk_li_tx foreign key ( transaction_id ) references transaction ( transaction_id ),
+   constraint fk_li_prod foreign key ( product_id ) references product ( product_id )
 );
 
 create table inventory (
-   inventory_id      number primary key
-      generated always as identity,
+   inventory_id number generated always as identity,
    store_id          number not null,
    product_id        number not null,
    reorder_threshold number not null check ( reorder_threshold >= 0 ),
    stock_level       number not null check ( stock_level >= 0 ),
-   constraint uq_inventory_store_prod unique ( store_id,
-                                               product_id ),
-   constraint fk_inv_store foreign key ( store_id )
-      references store ( store_id ),
-   constraint fk_inv_prod foreign key ( product_id )
-      references product ( product_id )
+   constraint pk_tansaction primary key ( transaction_id ),
+   constraint uq_inventory_store_prod unique ( store_id, product_id ),
+   constraint fk_inv_store foreign key ( store_id ) references store ( store_id ),
+   constraint fk_inv_prod foreign key ( product_id ) references product ( product_id )
 );
 
 create table supplier (
-   supplier_id   number primary key
-      generated always as identity,
+   supplier_id   number generated always as identity,
    supplier_name varchar2(100) not null,
    contact_info  varchar2(200),
-   constraint fk_sup foreign key ( product_id )
-      references product ( product_id )
+
+   constraint pk_supplier primary key ( supplier )
 );
